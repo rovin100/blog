903bf44baa7bd2decf19f6386d3fa82e19d7ef37
Rovin Varshney <rovin100@gmail.com>
2014-01-18 18:41:45 +0530

    Fix Spec for Dashboard#index

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe DashboardController do
 	it 'render text "Dashboard"  GET#index' do
 		get :index
-		expect(response).to be_success
+		expect(response.body).to match /Dashboard/m
 	end
 end
